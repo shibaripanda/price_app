e7956e961c4b70edf50875dc1349c542447ac7f2
unknown <remontf10@gmail.com>
2023-09-06 19:30:45 +0300

    drfd

--- modules/xlcToJson.js
+++ modules/xlcToJson.js
@@ -2,5 +2,9 @@ import xlsToJSON from "excel-to-clean-json"
 
 export default function xlcToJson(fileLink){
     const loop = xlsToJSON.rows(fileLink, 'Price')
-    console.log(loop[8139])
+    const newloop = loop.slice(8)
+    const result = newloop.filter(item => (item[4].split(' ')).includes('Y7') && item[3] == '+')
+    for (let i of result){
+        console.log(i[0] + ': ' + i[4] + ' ' + i[5] + ' Price: ' + i[6])
+    }
 }

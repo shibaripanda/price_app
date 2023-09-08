63082fa1bf596ba0d95d2bcfdb232666a5a483e8
shibaripanda <prevetlunatikam@gmail.com>
2023-09-09 01:29:29 +0300

    gfdgd

--- modules/getFileFromServer.js
+++ modules/getFileFromServer.js
@@ -6,7 +6,7 @@ export default async function getFileFromServerPogremuhi(nameFile, limit){
         let date = new Date()
         for (let i = 0; i < limit; date.setDate(date.getDate() - 1)){
             console.log('Test: ' + date.toLocaleDateString())
-            const res = await fetch('http://www.pogremuhi.com/pricelist/' + date.toLocaleDateString() + '.xls')
+            const res = await fetch('http://www.pogremuhi.com/pricelist/' + date.toLocaleDateString().split('/').join('.') + '.xls')
             if(res.statusText == 'OK'){
                 res.body.pipe(fs.createWriteStream(nameFile))
                 console.log('Done!' + ' ' + date.toLocaleDateString())
